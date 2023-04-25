7727f2ddb764c1395285a97f484054afd63c3d06
Elizabeth Doyle <myoddjourney@gmail.com>
2023-04-25 13:11:54 -0400

    9 passing tests out of 15

--- src/index.js
+++ src/index.js
@@ -42,12 +42,44 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
 console.log('project wired!')
 //header 
 var header = document.querySelector('header');
-console.log(header);
+//console.log(header);
+
 //nav
 var nav = document.querySelector('nav');
 console.log(nav);
 var headerNavLinks = document.querySelectorAll('header nav a');
 console.log(headerNavLinks)
+headerNavLinks.forEach(link => {
+  link.classList.add('italic')
+});
+
+//header links
+var servicesLink = document.querySelector('header nav a:nth-of-type(1)');
+servicesLink.textContent = siteContent["nav"]["nav-item-1"];
+
+var productLink = document.querySelector('header nav a:nth-of-type(2)');
+productLink.textContent = siteContent["nav"]["nav-item-2"];
+
+var visionLink = document.querySelector('header nav a:nth-of-type(3)');
+visionLink.textContent = siteContent["nav"]["nav-item-3"];
+
+var featuresLink = document.querySelector('header nav a:nth-of-type(4)');
+featuresLink.textContent = siteContent["nav"]["nav-item-4"];
+
+var aboutLink = document.querySelector('header nav a:nth-of-type(5)');
+aboutLink.textContent = siteContent["nav"]["nav-item-5"];
+
+var contactLink = document.querySelector('header nav a:nth-of-type(6)');
+contactLink.textContent = siteContent["nav"]["nav-item-6"];
+
+
+// var navItem2 = 
+// var navItem3 = 
+// var navItem4 = 
+// var navItem5 = 
+// var navItem6 = 
+
+
 
 
 // logo image
@@ -58,20 +90,22 @@ logoImg.src = siteContent["images"]["logo-img"];
 
 // cta section
 var ctaDiv = document.querySelector('.cta-text');
-console.log(ctaDiv.firstElementChild);
+//console.log(ctaDiv.firstElementChild);
 var ctaHeading = ctaDiv.firstElementChild; 
 ctaHeading.textContent = siteContent["cta"]["h1"];
-console.log(ctaHeading);
+//console.log(ctaHeading);
 var ctaParagraph = ctaDiv.lastElementChild
 ctaParagraph.textContent = siteContent["cta"]["button"];
 console.log(ctaParagraph)
-
 
 // cta image
 var ctaImg = document.querySelector('#cta-img')
 // const ctaImgAlt = document.querySelector('#cta-img')
 // ctaImgAlt = "Image of a code snippet";
 ctaImg.src = siteContent["images"]["cta-img"];
+
+// main content
+
 
 
 // middle image
